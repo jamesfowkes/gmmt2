f094a697b9be73518567e09448c4034472e7fb2e
James Fowkes <jamesfowkes@gmail.com>
2017-02-13 10:49:58 +0000

    Moved configuration to YAML file

--- gmmt2.py
+++ gmmt2.py
@@ -1,38 +1,47 @@
 """ gmmt2.py
 
 Usage:
-	gmmt2.py <app_id> <app_key>
+	gmmt2.py <config_file> <app_id> <app_key>
 """
 
 import docopt
 import logging
+import pprint
+
+import yaml
 
 import transportapi.bus
 import transportapi.train
 
 from journey import Journey
 
-def get_journeys_to_town(credentials):
-	hg_buses = transportapi.bus.get_live_json("3300BR0307", credentials)
-	hg_buses = transportapi.bus.flatten_services(hg_buses)
+def get_bus_journeys(config, credentials):
+	buses = transportapi.bus.get_live_json(config["stop_id"], credentials)
+	buses = transportapi.bus.flatten_services(buses)
+	buses = [Journey.from_bus_json(json) for json in buses["departures"]]
+	return buses
 
-	pr_buses = transportapi.bus.get_live_json("3300BR0270", credentials)
-	pr_buses = transportapi.bus.flatten_services(pr_buses)
-	
-	trains = transportapi.train.get_timetable_json_now("BEE", credentials, {"calling_at":"NOT"})
+def get_train_journeys(config, credentials):
+	trains = transportapi.train.get_timetable_json_now(config["station_code"], credentials, {"calling_at":config["calling_at"]})
+	return [Journey.from_train_json(json) for json in trains["departures"]["all"]]
 
-	hg_journeys = [Journey.from_bus_json(json) for json in hg_buses["departures"]]
-	pr_journeys = [Journey.from_bus_json(json) for json in pr_buses["departures"]]
-	train_journeys = [Journey.from_train_json(json) for json in trains["departures"]["all"]]
+def flatten(list_of_lists):
+	return [item for sublist in list_of_lists for item in sublist]
+
+def get_journeys(config, credentials):
+
+	bus_journeys = flatten([get_bus_journeys(bus_cfg, credentials) for bus_cfg in config["buses"]])
+	train_journeys = flatten([get_train_journeys(train_cfg, credentials) for train_cfg in config["trains"]])
 
 	all_journeys = []
+	for bus in bus_journeys:
+		all_journeys.append(bus)
 	
-	all_journeys.extend(hg_journeys)
-	all_journeys.extend(pr_journeys)
-	all_journeys.extend(train_journeys)
-
+	for train in train_journeys:
+		all_journeys.append(train)
+	
 	return sorted(all_journeys)
-
+	
 if __name__ == "__main__":
 
 	logging.basicConfig(level=logging.INFO)
@@ -44,7 +53,10 @@ if __name__ == "__main__":
 		"app_key": args["<app_key>"]
 	}
 
-	all_journeys = get_journeys_to_town(credentials)
+	with open(args["<config_file>"], 'r') as f:
+		config = yaml.load(f)
 
-	for j in sorted(all_journeys):
+	all_journeys = get_journeys(config, credentials)
+
+	for j in all_journeys:
 		print(j)
