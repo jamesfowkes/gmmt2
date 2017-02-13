8e4be83ba5216c4458fd8159f3b7ee8b8a70e70e
James Fowkes <jamesfowkes@gmail.com>
2017-02-13 10:50:18 +0000

    Transport API module uses CacheControl

--- transportapi/__init__.py
+++ transportapi/__init__.py
@@ -4,10 +4,13 @@ import logging
 
 TRANSPORT_API_URL = os.getenv("TRANSPORT_API_URL")
 
+import cachecontrol
+sess = cachecontrol.CacheControl(requests.Session())
+
 def request(target, url_params):
 	full_url = TRANSPORT_API_URL + target
 	
-	r = requests.get(full_url, params=url_params)
+	r = sess.get(full_url, params=url_params)
 
 	logging.info("Requesting {}".format(r.url))
 
