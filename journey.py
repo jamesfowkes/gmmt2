43f569e765e152c7400bb616135c78b2d391265f
James Fowkes <jamesfowkes@gmail.com>
2017-02-13 18:24:10 +0000

    Add minutes to departure to journey class

--- journey.py
+++ journey.py
@@ -11,12 +11,14 @@ class Journey(namedtuple("Journey", ["service", "time"])):
 	def from_bus_json(cls, json):
 		service = json["line"]
 		time = datetime.datetime.strptime(json["best_departure_estimate"], "%H:%M").time()
+		time = datetime.datetime.combine(datetime.datetime.today(), time)
 		return cls(service, time)
 
 	@classmethod
 	def from_train_json(cls, json):
 		service = "Train"
 		time = datetime.datetime.strptime(json["aimed_departure_time"], "%H:%M").time()
+		time = datetime.datetime.combine(datetime.datetime.today(), time)
 		return cls(service, time)
 
 	def json(self):
@@ -27,6 +29,10 @@ class Journey(namedtuple("Journey", ["service", "time"])):
 			}
 		)
 
+	def mins_to_depart(self, timenow=datetime.datetime.now()):
+		minutes = (self.time - timenow).total_seconds()/60
+		return int(minutes + 0.5)
+		
 	def __str__(self):
 		return "{} at {:%H:%M}".format(self.service, self.time)
 
