8134803eecbd42c17d7b3759e528a29c4160745a
James Fowkes <jamesfowkes@gmail.com>
2017-02-27 22:21:49 +0000

    Add public and debug options to args

--- runapp.py
+++ runapp.py
@@ -1,7 +1,7 @@
 """ runapp.py
 
 Usage:
-	runapp.py <config_file>
+	runapp.py <config_file> [--public] [--debug]
 """
 
 import docopt
@@ -15,10 +15,10 @@ from gmmt2app import app
 
 if __name__ == "__main__":
 
-	if app.debug:
-		logging.basicConfig(level=logging.INFO)
-
 	args = docopt.docopt(__doc__)
+
+	if args["--debug"]:
+		logging.basicConfig(level=logging.INFO)
 
 	with open(args["<config_file>"], 'r') as f:
 		config = yaml.load(f)
@@ -33,4 +33,7 @@ if __name__ == "__main__":
 
 	app.config["gmmt2"] = config
 
-	app.run()
+	if args['--public']:
+		app.run(host='0.0.0.0')
+	else:
+		app.run()
